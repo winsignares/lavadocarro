9db40b6908b9acb9507dab8cbdfe319571ceaf81
jasson2558 <jdescorcia4@misena.edu.co>
2023-04-21 13:42:23 -0500

    correcciones tablas

--- CARW/api/venta.py
+++ CARW/api/venta.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, request, jsonify, json
 from db import db, app, ma
 from flask import Flask,  redirect, request, jsonify, json, session, render_template
-from model.usuarios  import usuarios, usuariosSchema
+from model.ventas import ventas, ventasSchema
 
-routes_usuarios = Blueprint("routes_usuarios", __name__)
+routes_ventas = Blueprint("routes_ventas", __name__)
 
-usuario_schema = usuariosSchema()
-usuarios_schema = usuariosSchema(many=True)
+venta_schema = ventasSchema()
+ventas_schema = ventasSchema(many=True)
