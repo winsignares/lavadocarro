80fa76d242f138fb1118705259df3b6bfdad5bff
kenny1303 <arevalosarmientokenny@gmail.com>
2023-04-25 16:46:48 -0500

    provando consultas

--- CARW/rutas/login.py
+++ CARW/rutas/login.py
@@ -1,5 +1,7 @@
 from db import db, app, ma
 from flask import Blueprint, Flask,  redirect, request, jsonify, json, session, render_template
+from model.usuarios  import usuarios, usuariosSchema
+from model.roles  import roles, rolesSchema
 
 routes_login = Blueprint("routes_login", __name__)
 
@@ -7,3 +9,20 @@ routes_login = Blueprint("routes_login", __name__)
 def indexlogin():
     
     return render_template('/main/login.html')
+
+@routes_login.route('/consulusuario', methods=['GET'])
+def consullist():
+    datos= {}
+    resultado = db.session.query(usuarios, roles).select_from(usuarios).join(roles).all()
+    users = []
+    i = 0
+    for usuarios, roles in resultado:
+        i += 1
+        datos[i] = {
+		'nombreu':usuarios.Usuario,
+		'password':usuarios.Contraseña,
+		'rolsito': usuarios.id_rol                     
+        }
+    users.append(datos)
+    print(users)
+    return jsonify(datos)
--- CARW/static/js/login.js
+++ CARW/static/js/login.js
@@ -1,2 +1,9 @@
 const usuario = document.getElementById("nombre");
 const contra = document.getElementById("contraseña");
+
+function ingresar() {
+    axios.get('consulusuario', {
+            responseType: 'json'
+        })
+        .then()
+}
