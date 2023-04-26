8e571691e00d55ff16049a268db84f94631705be
kenny1303 <arevalosarmientokenny@gmail.com>
2023-04-26 15:19:28 -0500

    TwT

--- CARW/static/js/login.js
+++ CARW/static/js/login.js
@@ -6,12 +6,16 @@ function ingresar() {
             responseType: 'json'
         })
         .then(function(response) {
-            let nombreu = usuario.value
             let datos = response.data
-            if (datos.hasOwnProperty(nombreu.value) && datos.password.value == contra.value) {
-                window.alert("esta chinga funciona we")
+            let nombreu = usuario.value;
+            let password = contra.value;
+            for (let i = 1; i <= Object.keys(datos).length; i++) {
+                if (datos[i].nombreu === nombreu && datos[i].password === password) {
+                    window.alert("esta chinga funciona we")
+                    return;
+                }
             }
-
+            window.alert("Usuario o contraseña incorrectos");
         })
         .catch(function(error) {
             console.log(error);
