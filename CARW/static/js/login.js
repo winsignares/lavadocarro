66dfd71cd7293a3650e7370083f2945e6f8e1aa0
kenny1303 <arevalosarmientokenny@gmail.com>
2023-04-26 14:39:49 -0500

    ya no se

--- CARW/static/js/login.js
+++ CARW/static/js/login.js
@@ -1,30 +1,14 @@
 const usuario = document.getElementById("nombre");
 const contra = document.getElementById("contraseña");
 
-function ingresar2() {
-    axios.get('consulusuario', {
-            responseType: 'json'
-        })
-        .then(function(response) {
-            let datos = response.data
-            if (nombreu in datos == usuario && password in datos == contra) {
-                window.alert("esta chinga funciona we")
-            }
-
-        })
-
-    .catch(function(error) {
-        console.log(error);
-    });
-}
-
 function ingresar() {
     axios.get('consulusuario', {
             responseType: 'json'
         })
         .then(function(response) {
+            nombreu = usuario
             let datos = response.data
-            if (datos.hasOwnProperty(usuario.value) && datos[usuario.value] == contra.value) {
+            if (datos.hasOwnProperty(nombreu.value) && datos[password.value] == contra.value) {
                 window.alert("esta chinga funciona we")
             }
 
