7434366dca402fb3f499850153532c894af0a164
kenny1303 <arevalosarmientokenny@gmail.com>
2023-05-03 17:04:00 -0500

    no se

--- CARW/static/js/Ppredeterminados.js
+++ CARW/static/js/Ppredeterminados.js
@@ -10,16 +10,22 @@ function ver_paquetes() {
             var length = (Object.keys(datos).length) + 1;
             let opciones = '';
             for (let index = 1; index < length; index++) {
-                opciones += `
-          <div class="card">
-            <img src="{{url_for('static', filename='img/logo.jpg')}}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${datos[index].titulo}</h5>
-              <p class="card-text">${datos[index].descripcion}</p>
-              <p class="card-text">${datos[index].valor}</p>
-              <a href="#" class="btn btn-primary">Seleccionar</a>
+                opciones +=
+                    `<div class="mi-div d-flex flex-wrap col-md-5" style="max-width: 100%;">
+                    <div class="col-md-4">
+                        <img src="/static/img/logo.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${datos[index].titulo}</h5>
+                            <p class="card-text">${datos[index].descripcion}</p>
+                            <p class="card-text">$${datos[index].valor}</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>`;
+            </br>`;
             }
             remplazo.innerHTML = opciones;
         })
