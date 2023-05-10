fc597e5d98b830c2ec430f15efa106ff915331bc
kenny1303 <arevalosarmientokenny@gmail.com>
2023-05-10 16:31:29 -0500

    a

--- CARW/static/js/Pagos.js
+++ CARW/static/js/Pagos.js
@@ -1,7 +1,10 @@
 let Lusuarios = document.getElementById('lusuarios');
+let Lpaquetes = document.getElementById('lpaquetes');
+let Cmatri = document.getElementById('matri');
+
 // odtener los usuarios inicio
-function ver_procedimientos() {
-    axios.get('fronted/consulprocedimientos', {
+function ver_usuarios() {
+    axios.get('fronted/consulusuario', {
         responseType: 'json'
     })
 
@@ -11,7 +14,7 @@ function ver_procedimientos() {
             let opciones = '';
             for (let index = 1; index < length; index++) {
                 opciones +=
-                    `<option id="${index}" value="1">${datos[index].nombreu}</option>`;
+                    `<option id="${index}" value="${index}">${datos[index].nombreu}</option>`;
             }
             Lusuarios.innerHTML = opciones;
         })
@@ -19,4 +22,29 @@ function ver_procedimientos() {
             console.log(error);
         });
 }
-// odtener los procedimientos fin
+// odtener los usuarios fin
+
+// odtener los paquetes inicio
+function ver_paquetes() {
+    axios.get('fronted/consulpaquetes', {
+        responseType: 'json'
+    })
+
+    .then(function(response) {
+            let datos = response.data
+            var length = (Object.keys(datos).length) + 1;
+            let opciones = '';
+            for (let index = 1; index < length; index++) {
+                opciones +=
+                    `
+                    
+                    <option selected>Lista Paquetes</option>
+                    <option id="${index}" value="${index}">${datos[index].titulo}</option>`;
+            }
+            Lpaquetes.innerHTML = opciones;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+// odtener los paquetes fin
