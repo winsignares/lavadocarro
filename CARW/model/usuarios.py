a2cef713d056540f9db4fe8f3be9eb6c54c689ea
jasson2558 <jdescorcia4@misena.edu.co>
2023-05-12 09:44:57 -0500

    correccion de tablas

--- CARW/model/usuarios.py
+++ CARW/model/usuarios.py
@@ -7,17 +7,19 @@ class usuarios(db.Model):
     Usuario = db.Column(db.String(50), primary_key=True)
     Nombre = db.Column(db.String(50))
     Apellido = db.Column(db.String(50))
+    Cedula = db.Column(db.int)
     Correo = db.Column(db.String(100))
-    Telefono = db.Column(db.Integer)
+    Telefono = db.Column(db.bigint)
     Contraseña = db.Column(db.String(20))
     id_vehiculo = db.Column(db.String(50), db.ForeignKey('tblvehiculos.Matricula'))
     id_rol = db.Column(db.Integer, db.ForeignKey('tblroles.id'))
     
 
-    def __init__(self, Nombre,Apellido,Correo,Telefono,Contraseña,id_vehiculo,id_rol ):
+    def __init__(self, Nombre,Apellido,Cedula,Correo,Telefono,Contraseña,id_vehiculo,id_rol ):
         
         self.Nombre = Nombre
         self.Apellido = Apellido
+        self.Cedula = Cedula
         self.Correo = Correo
         self.Telefono = Telefono
         self.Contraseña = Contraseña
@@ -30,4 +32,4 @@ with app.app_context():
 
 class usuariosSchema(ma.Schema):
     class Meta:
-        fields = ('Usuario','Nombre','Apellido','Correo','Telefono','Contraseña','id_vehiculo','id_rol')
+        fields = ('Usuario','Nombre','Apellido','Cedula','Correo','Telefono','Contraseña','id_vehiculo','id_rol')
