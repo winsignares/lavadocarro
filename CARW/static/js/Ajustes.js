57ff98f8f7f4d1cf8852bbaf7134936c75498843
jasson2558 <jdescorcia4@misena.edu.co>
2023-05-29 22:22:36 -0500

    ajutes parte visual 100%

--- CARW/static/js/Ajustes.js
+++ CARW/static/js/Ajustes.js
@@ -1,19 +1,49 @@
+function mostrarmenuUS() {
+    document.getElementById('ajustes-botones2').style.visibility = 'visible';
+    document.getElementById('ajustes-botones3').style.visibility = 'hidden';
+    document.getElementById('ajvehiculos').style.visibility = 'hidden';
+    document.getElementById('ajusuarios').style.visibility = 'hidden';
+    document.getElementById('ajpaquetes').style.visibility = 'hidden';
+    document.getElementById('ajeliminar').style.visibility = 'hidden';
+    document.getElementById('ajeliminarPQ').style.visibility = 'hidden';
+}
+
+function mostrarmenuPQ() {
+    document.getElementById('ajustes-botones3').style.visibility = 'visible';
+    document.getElementById('ajustes-botones2').style.visibility = 'hidden';
+    document.getElementById('ajvehiculos').style.visibility = 'hidden';
+    document.getElementById('ajusuarios').style.visibility = 'hidden';
+    document.getElementById('ajpaquetes').style.visibility = 'hidden';
+    document.getElementById('ajeliminar').style.visibility = 'hidden';
+    document.getElementById('ajeliminarPQ').style.visibility = 'hidden';
+    document.getElementById('ajregistrarPQ').style.visibility = 'hidden';
+}
+
 function mostrarRvehiculos() {
     document.getElementById('ajvehiculos').style.visibility = 'visible';
     document.getElementById('ajusuarios').style.visibility = 'hidden';
     document.getElementById('ajpaquetes').style.visibility = 'hidden';
+    document.getElementById('ajeliminar').style.visibility = 'hidden';
+    document.getElementById('ajeliminarPQ').style.visibility = 'hidden';
+    document.getElementById('ajregistrarPQ').style.visibility = 'hidden';
 }
 
 function mostrarRuarios() {
     document.getElementById('ajusuarios').style.visibility = 'visible';
     document.getElementById('ajvehiculos').style.visibility = 'hidden';
     document.getElementById('ajpaquetes').style.visibility = 'hidden';
+    document.getElementById('ajeliminar').style.visibility = 'hidden';
+    document.getElementById('ajeliminarPQ').style.visibility = 'hidden';
+    document.getElementById('ajregistrarPQ').style.visibility = 'hidden';
 }
 
 function mostrarRpaquetes() {
     document.getElementById('ajpaquetes').style.visibility = 'visible';
     document.getElementById('ajusuarios').style.visibility = 'hidden';
     document.getElementById('ajvehiculos').style.visibility = 'hidden';
+    document.getElementById('ajeliminar').style.visibility = 'hidden';
+    document.getElementById('ajeliminarPQ').style.visibility = 'hidden';
+    document.getElementById('ajregistrarPQ').style.visibility = 'hidden';
 }
 
 function eliminarRuarios() {
@@ -21,4 +51,25 @@ function eliminarRuarios() {
     document.getElementById('ajusuarios').style.visibility = 'hidden';
     document.getElementById('ajvehiculos').style.visibility = 'hidden';
     document.getElementById('ajpaquetes').style.visibility = 'hidden';
+    document.getElementById('ajeliminarPQ').style.visibility = 'hidden';
+    document.getElementById('ajregistrarPQ').style.visibility = 'hidden';
 }
+
+function eliminarpaquetes() {
+    document.getElementById('ajeliminarPQ').style.visibility = 'visible';
+    document.getElementById('ajusuarios').style.visibility = 'hidden';
+    document.getElementById('ajvehiculos').style.visibility = 'hidden';
+    document.getElementById('ajpaquetes').style.visibility = 'hidden';
+    document.getElementById('ajeliminar').style.visibility = 'hidden';
+    document.getElementById('ajregistrarPQ').style.visibility = 'hidden';
+}
+
+function mostrarpaquetesPQ() {
+    document.getElementById('ajregistrarPQ').style.visibility = 'visible';
+    document.getElementById('ajusuarios').style.visibility = 'hidden';
+    document.getElementById('ajvehiculos').style.visibility = 'hidden';
+    document.getElementById('ajpaquetes').style.visibility = 'hidden';
+    document.getElementById('ajeliminar').style.visibility = 'hidden';
+    document.getElementById('ajeliminarPQ').style.visibility = 'hidden';
+}
+ajregistrarPQ
